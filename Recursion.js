702e4fd822297e406ad32de406dac7f65d797edd
BrossCode <89676904+BrossCode@users.noreply.github.com>
2021-10-21 18:28:41 -0700

    Recursion Setup

---
+++ Recursion.js
@@ -0,0 +1,15 @@
+// Recursion
+
+var arr = [5,2,3,8,9,6,12,67,89,125,82];
+
+function arrSort(array) {
+    let storage;
+    for (let i = 0; i < array.length; i++){
+        if (array[i-1] > array[i]){
+            storage = array[i];
+            array[i] = array[i-1];
+            array[i-1] = storage;
+        }
+    }
+    console.log(arr);
+}
