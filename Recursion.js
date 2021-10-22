2cd5dec025ebf00b71804f52cc3d78fdba663de3
BrossCode <89676904+BrossCode@users.noreply.github.com>
2021-10-21 18:32:15 -0700

    whoops

--- Recursion.js
+++ Recursion.js
@@ -3,15 +3,17 @@
 var arr = [5,2,3,8,9,6,12,67,89,125,82];
 
 function hello() {
-    function arrSort(array) {
-        let storage;
-        for (let i = 0; i < array.length; i++){
-            if (array[i-1] > array[i]){
-                storage = array[i];
-                array[i] = array[i-1];
-                array[i-1] = storage;
-            }
+    arrSort(arr);
+}
+
+function arrSort(array) {
+    let storage;
+    for (let i = 0; i < array.length; i++){
+        if (array[i-1] > array[i]){
+            storage = array[i];
+            array[i] = array[i-1];
+            array[i-1] = storage;
         }
-        console.log(arr);
     }
+    console.log(arr);
 }
