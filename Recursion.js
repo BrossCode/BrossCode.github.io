b7100129f508902f2bf2fd4cc747b531e6ea19cd
BrossCode <89676904+BrossCode@users.noreply.github.com>
2021-10-21 23:58:08 -0700

    lets try this?

--- Recursion.js
+++ Recursion.js
@@ -6,19 +6,47 @@ function hello() {
     arrSort(arr);
 }
 
+// storage vars for pointers
+let i = 0;
+let j = arr.length-1;
+// leftmost
+let storageLeft = arr[i];
+// rightmost
+let storageRight = arr[j];
+// pivot
+let pivot = arr[arr.length/2];
+
 function arrSort(array) {
-    let storage;
-    for (let i = 0; i < array.length; i++){
-        if (array[i-1] > array[i]){
-            storage = array[i];
-            array[i] = array[i-1];
-            array[i-1] = storage;
-            console.log(i);
-            if (i > 0){
-            i = i-1;
-            console.log(i);
-            }
-        }
+    // compare pivot point to leftmost var
+    // if left is higher and right is lower
+    if (pivot < storageLeft && pivot > storageRight){
+        // swap values since they can be flipped around pointer
+        array[i] = storageRight;
+        array[j] = storageLeft;
     }
-    console.log(arr);
+    if (pivot > storageLeft){
+        // move over left pointer
+        i += 1;
+        // change value stored
+        storageLeft = array[i];
+        }
+    if (pivot < storageRight){
+        // move over right pointer
+        j -= 1;
+        // change value stored
+        storageRight = array[j];
+    }
+    // end command
+    if (i == array.length/2 && j == array.length/2){
+        //log it
+        console.log(array);
+        // return it
+        return array;
+    }
+    else {
+        // log it
+        console.log(array);
+        // call function again
+        arrSort(array);
+    }
 }
