43d8dfe8f46a7a46561c66f9eee253f80c2b27ce
BrossCode <89676904+BrossCode@users.noreply.github.com>
2021-10-23 14:02:12 -0700

    Update Recursion.js

--- Recursion.js
+++ Recursion.js
@@ -2,15 +2,8 @@
 
 var arr = [5,2,3,8,9,6,12,67,89,125,82];
 
-// storage vars for pointers
-let i = 0;
-let j = arr.length-1;
-// leftmost
-let storageLeft = arr[i];
-// rightmost
-let storageRight = arr[j];
-// pivot
-let pivot = arr[Math.floor(arr.length/2)];
+let storageCurrent;
+let storageAtValue;
 
 function hello() {
     arrSort(arr);
@@ -18,59 +11,19 @@ function hello() {
 
 // gotta add a tracking array that can be changed
 function arrSort(array) {
-    // storage array
-    let tempArr = array;
-    console.log(tempArr);
-    console.log(pivot);
-    console.log(storageLeft);
-    console.log(storageRight);
-    // compare pivot point to leftmost var
-    // if left is higher and right is lower
-    if (pivot < storageLeft && pivot > storageRight){
-        // swap values since they can be flipped around pointer
-        tempArr[i] = storageRight;
-        tempArr[j] = storageLeft;
-
-        // log it
-        console.log(tempArr);
-        // swap it
-        array = tempArr;
-        // log again
-        console.log("next iteration array " + array);
-        // call function again
-        arrSort(array);
-    }
-    if (pivot > storageLeft){
-        // move over left pointer
-        i += 1;
-        console.log(i);
-        // change value stored
-        storageLeft = tempArr[i];
-        console.log(storageLeft);
+    // grab a value
+    for (let i = 0; i < arr.length; i++){
+        // setstorage to it
+        storageCurrent = array[i];
+        // search through the next var
+        for (let j = 0; j < arr.length; j++){
+            // compare storage to other var
+            if (storageCurrent > array[j]){
+                // swap
+                arr[i] = arr[j];
+                arr[j] = storageCurrent;
+            }
         }
-    if (pivot < storageRight){
-        // move over right pointer
-        j -= 1;
-        console.log(j);
-        // change value stored
-        storageRight = tempArr[j];
-        console.log(storageRight);
     }
-    // end command
-    if (i == pivot && j == pivot){
-        //log it
-        console.log(tempArr);
-        // return it
-        return tempArr;
-    }
-    else {
-        // log it
-        console.log(tempArr);
-        // swap it
-        array = tempArr;
-        // log again
-        console.log("next iteration array " + array);
-        // call function again
-        arrSort(array);
-    }
+    arrSort(arr);
 }
