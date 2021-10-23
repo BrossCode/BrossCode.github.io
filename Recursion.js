28ed26e0d0b28e9d2638b0ac49072dabbec5989d
BrossCode <89676904+BrossCode@users.noreply.github.com>
2021-10-23 14:17:08 -0700

    Update Recursion.js

--- Recursion.js
+++ Recursion.js
@@ -12,20 +12,21 @@ function hello() {
 
 // gotta add a tracking array that can be changed
 function arrSort() {
-    // setstorage to it
-    storageCurrent = arr[i];
-    console.log(storageCurrent);
-    i += 1;
-    // search through the next var
-    for (let j = 0; j < arr.length; j++){
-        // compare storage to other var
-        if (storageCurrent > arr[j]){
-            // swap
-            console.log(arr[j]);
-            console.log(arr[i]);
-            arr[i] = arr[j];
-            arr[j] = storageCurrent;
-            break;
+    for (let i = 0; i < arr.length; i++){
+        // setstorage to it
+        storageCurrent = arr[i];
+        console.log(storageCurrent);
+        // search through the next var
+        for (let j = 0; j < arr.length; j++){
+            // compare storage to other var
+            if (storageCurrent > arr[j]){
+                // swap
+                console.log(arr[j]);
+                console.log(arr[i]);
+                arr[i] = arr[j];
+                arr[j] = storageCurrent;
+                break;
+            }
         }
     }
     console.log(arr);
